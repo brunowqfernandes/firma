0a46f41347a9498eb4530a635843cafe2d826285
Bruno Fernandes <bruno.wqfernandes@gmail.com>
2024-03-31 12:26:20 -0300

    .

--- wp-content/themes/firma/main.js
+++ wp-content/themes/firma/main.js
@@ -1,18 +1,11 @@
 window.onload = (e) => {
-  var image = document.getElementsByClassName('parallax');
-
-  new simpleParallax(image, {
-      delay: 1,
-      transition: 'cubic-bezier(0,0,0,1)',
-      orientation: 'up right'
-  });
+  const menuButton = document.querySelector('.menu-toggle');
+  const menuWrapper = document.querySelector('.menu-wrapper');
+  menuButton.addEventListener('click', function () {
+    menuWrapper.classList.toggle('open');
+  })
 }
 
 // Menu fixo quando scrolla
 window.onscroll = (e) => {
-  const header = document.querySelector('header')
-  if(window.scrollY > 100)
-      header.classList.add('fixed')
-  else
-      header.classList.remove('fixed')
 }
