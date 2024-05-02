6c9ef36a579a4ae6526d3f4327b75e8f649f59f5
Bruno Fernandes <bruno.wqfernandes@gmail.com>
2024-05-02 13:09:07 -0300

    .

--- wp-content/themes/firma/page-time.php
+++ wp-content/themes/firma/page-time.php
@@ -3,8 +3,12 @@
 ?>
 
 <main class="time">
-    <div class="descricao"></div>
-    <div class="foto"></div>
+    <div class="descricao">
+        <p><?php echo nl2br(get_field('descricao', false, false)) ?></p>
+    </div>
+    <div class="foto" style="background: url('<?php echo get_field('foto') ?>')">
+        <h2><?php echo get_the_title(); ?></h2>
+    </div>
 </main>
 
 <?php
