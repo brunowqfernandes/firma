2a421672abe95c8daa370563ff38f940e64eb450
Bruno Fernandes <bruno.wqfernandes@gmail.com>
2024-05-03 14:20:22 -0300

    a

--- wp-content/themes/firma/page-sobre.php
+++ wp-content/themes/firma/page-sobre.php
@@ -2,6 +2,16 @@
   get_header();
 ?>
 
+<style>
+  @media (max-width: 767px) {
+    .hero {
+      padding-top: 120px;
+      padding-bottom: 40px;
+      height: auto;
+    }
+  }
+</style>
+
 <main>
   <h2 class="titulo-pagina" style="color:#fff;"><?php the_title()?></h2>
   <div class="hero" style="background-image: url(<?php echo get_field('background_sobre')?>)">
